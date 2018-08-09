93625ae6bbb9ac657b4bfb73446a1b24ff379ba5
Jan Boddez <jan@janboddez.be>
2018-08-09 21:12:13 +0200

    Re-simplified (de)activation hook

--- go-html-suffix.php
+++ go-html-suffix.php
@@ -37,8 +37,6 @@ class GO_Html_Suffix {
 	 */
 	public function activate() {
 		global $wp_rewrite;
-		// Store old page permastruct.
-		update_option( 'go_html_suffix_old_page_permastruct', $wp_rewrite->get_page_permastruct() );
 		$this->set_page_permalink();
 		$wp_rewrite->flush_rules();
 	}
@@ -50,12 +48,9 @@ class GO_Html_Suffix {
 	 */
 	public function deactivate() {
 		global $wp_rewrite;
-		$old_page_permastruct = get_option( 'go_html_suffix_old_page_permastruct', false );
+		$page_permastruct = $wp_rewrite->get_page_permastruct();
 
-		if ( false !== $old_page_permastruct ) {
-			// Restore original URL structure.
-			$wp_rewrite->page_structure = $old_page_permastruct;
-		} elseif ( '.html' === substr( $page_permastruct, -5 ) ) {
+		if ( false !== $page_permastruct && '.html' === substr( $page_permastruct, -5 ) ) {
 			// Remove '.html' from end of URLs.
 			$wp_rewrite->page_structure = substr( $page_permastruct, 0, -5 );
 		}
@@ -72,7 +67,7 @@ class GO_Html_Suffix {
 		global $wp_rewrite;
 		$page_permastruct = $wp_rewrite->get_page_permastruct();
 
-		if ( '.html' !== substr( $page_permastruct, -5 ) ) {
+		if ( false !== $page_permastruct && '.html' !== substr( $page_permastruct, -5 ) ) {
 			$wp_rewrite->page_structure = $page_permastruct . '.html';
 		}
 	}
